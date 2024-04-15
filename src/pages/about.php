ffa63bd641609d5bb65b23f4fb3e50a477cc9a47
Hardwood Vazquez <s.vazquez6363@gmail.com>
2024-04-15 01:00:57 -0400

    pathes

--- src/pages/about.php
+++ src/pages/about.php
@@ -3,21 +3,21 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="./js/jquery-3.7.1.min.js"></script>
-  <link rel="stylesheet" href="./foundation/css/foundation.css">
+  <script src="../../js/jquery-3.7.1.min.js"></script>
+  <link rel="stylesheet" href="../../foundation/css/foundation.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="./index.css">
-  <title>About Us</title>
+  <link rel="stylesheet" href="../../index.css">
+  <title>Checklist Page</title>
 </head>
 
 <body>
   <div class="topnav" id="myTopnav">
-      <a href="./src/pages/index.php" class="active">RIVAS PINESTRAW</a>
-      <a href="./src/pages/about.php">About Us</a>
-      <a href="./src/pages/order-form.php">Order Form</a>
-      <a href="./src/pages/checklist.php">Checklist</a>
-      <a href="./src/pages/contact.php">Contact Us</a>
-      <a href="./src/pages/sign-on.php">Sign In</a>
+      <a href="./index.php" class="active">RIVAS PINESTRAW</a>
+      <a href="./about.php">About Us</a>
+      <a href="./order-form.php">Order Form</a>
+      <a href="./checklist.php">Checklist</a>
+      <a href="./contact.php">Contact Us</a>
+      <a href="./sign-on.php">Sign In</a>
       <a href="javascript:void(0);" class="icon" onclick="hideNav()">
           <i class="fa fa-bars"></i>
       </a>
