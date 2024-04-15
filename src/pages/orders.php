3fac3197b7ec2984798f99b6fa8ca93dc7944ee9
Hardwood Vazquez <s.vazquez6363@gmail.com>
2024-04-15 02:38:02 -0400

    Update orders.php

--- src/pages/orders.php
+++ src/pages/orders.php
@@ -1,1 +1,60 @@
-orders
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="../../js/jquery-3.7.1.min.js"></script>
+  <link rel="stylesheet" href="../../foundation/css/foundation.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../../index.css">
+  <title>Orders</title>
+</head>
+<?php
+define( 'DB_NAME', 'rivas-lands' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'CoolCat2024' );
+define( 'DB_HOST', 'localhost' );
+
+?>
+<body>
+  <div class="topnav" id="myTopnav">
+      <a href="../../index.php" class="active">RIVAS PINESTRAW</a>
+      <a href="./about.php">About Us</a>
+      <a href="./order-form.php">Order Form</a>
+      <a href="./checklist.php">Checklist</a>
+      <a href="./contact.php">Contact Us</a>
+      <a href="./sign-on.php">Sign In</a>
+      <a href="javascript:void(0);" class="icon" onclick="hideNav()">
+          <i class="fa fa-bars"></i>
+      </a>
+  </div>
+  <br><br>
+  
+  <div id=showOrders></div>
+
+  <script>
+        function hideNav() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+
+        function deleteOrders(id) {
+                $.get("../ajax.php",{"cmd": "delete", "id" : id}, function(data) {
+                    $("#showOrder").html(data);
+                });
+                return(false);
+            }
+            function showOrders() {
+                $.get("../ajax.php",{"cmd": "show"}, function(data) {
+                    $("#showOrder").html(data);
+                });
+                return(false);
+            }
+            showOrders();
+    </script>
+</body>
+</html>
