cfca1b66d428614640fda230920ce0932825ddfd
Hardwood Vazquez <s.vazquez6363@gmail.com>
2024-04-14 22:38:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="./js/jquery-3.7.1.min.js"></script>
     <link rel="stylesheet" href="./foundation/css/foundation.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./index.css">
     
     <title>Rivas Pinestraw</title>
