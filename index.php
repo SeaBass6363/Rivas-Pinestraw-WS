c164cec7c568ec57e2a0ec7429eb8ef9dce8fac9
Hardwood Vazquez <s.vazquez6363@gmail.com>
2024-04-14 23:27:36 -0400

    style

--- index.php
+++ index.php
@@ -38,6 +38,7 @@
                 <a href="#order">Order Form</a>
                 <a href="#checklist">Checklist</a>
                 <a href="#contact">Contact Us</a>
+                <a href="#signin">Sign In</a>
                 <a href="javascript:void(0);" class="icon" onclick="hideNav()">
                     <i class="fa fa-bars"></i>
                 </a>
@@ -52,15 +53,7 @@
         <div class="grid-x">
             <div id="sidebar" class="cell small-12 medium-4 large-4 text-right">
                 <p id="text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam facilisis purus mi, eu lacinia ante semper vitae. Sed metus felis, pretium quis semper volutpat, aliquet non mauris. Etiam maximus enim vitae metus posuere, at cursus tellus pellentesque. Nunc lobortis varius est. Morbi ac neque nec erat vehicula cursus vel a tortor. Curabitur fermentum lectus eget risus vulputate, vel tincidunt justo ultricies. Sed sed ante ex. Quisque vitae quam at erat commodo mattis et non ipsum. Phasellus ac ultrices eros. Morbi vel ultrices justo. Nunc at aliquam ex. Etiam ornare in velit ut feugiat. Nulla urna dolor, placerat id dictum eget, sodales ut risus. Praesent at dictum ligula.
-
-    Ut accumsan felis a dapibus porta. Suspendisse iaculis ligula tortor, non bibendum velit eleifend eget. Proin luctus sed arcu sed molestie. Duis sed nulla porta, faucibus massa in, euismod elit. Fusce purus purus, eleifend at magna imperdiet, posuere consectetur nulla. Cras pharetra, purus non pellentesque congue, enim lacus varius nisi, non luctus libero mi eu sapien. Morbi et augue ut nisi elementum pulvinar eget non nunc. Sed in lacinia mi. In bibendum posuere leo, at posuere ex eleifend in. Aliquam erat volutpat.
-
-    Pellentesque vehicula diam venenatis massa aliquet lacinia. Quisque a gravida quam. Integer iaculis consectetur mauris. Morbi sit amet tincidunt dui. Integer malesuada rutrum nisl ac tincidunt. Morbi vitae convallis ipsum. Phasellus vulputate ornare nisi, eget maximus mauris imperdiet nec. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam tincidunt sapien ac leo tincidunt, eget aliquam nulla hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquet purus tellus, vel venenatis nulla tincidunt dictum. Proin vel dui justo. Quisque sit amet nisi nisi. Aenean sagittis, ipsum id convallis rhoncus, sem sem aliquet urna, vitae interdum eros libero sit amet nulla.
-
-    Phasellus in lorem convallis, efficitur orci quis, iaculis nisi. Duis eget augue nulla. Phasellus ultrices dolor ut ante egestas bibendum. Nullam sit amet augue eleifend, fermentum leo et, vestibulum risus. Etiam ac dui odio. Donec justo mi, convallis ac viverra in, suscipit elementum sapien. Etiam congue metus sit amet urna venenatis maximus. Donec vitae diam et nulla blandit lacinia. Nullam tristique leo risus, nec hendrerit erat faucibus vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-
-    Mauris quis eros vulputate, euismod ex quis, posuere massa. Donec sed mi et metus iaculis varius et vitae nibh. Aliquam id dapibus orci, ac consectetur mauris. Mauris dapibus tortor eget ante ullamcorper, nec finibus ante tempus. Mauris semper fringilla nulla ac blandit. Phasellus tristique, velit vitae malesuada rhoncus, nisl felis volutpat sapien, vel consequat ante tortor ornare nunc. Nulla vel augue fringilla, posuere felis nec, iaculis augue. Mauris bibendum est nibh, in ultricies lorem rutrum nec. Maecenas sit amet nisi urna. Vivamus finibus lectus id arcu varius vulputate. Donec accumsan tortor orci, vel bibendum dolor auctor id.
+                    
                 </p>
             </div>
             <div id="body" class="cell small-12 medium-8 large-8 text-right">
