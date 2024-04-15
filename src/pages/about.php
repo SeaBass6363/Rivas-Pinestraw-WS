56f12f9c5a9380962e72118a74f87518ec96fbf2
Hardwood Vazquez <s.vazquez6363@gmail.com>
2024-04-15 09:48:07 -0400

    Update about.php

--- src/pages/about.php
+++ src/pages/about.php
@@ -10,7 +10,7 @@
   <title>Checklist Page</title>
 </head>
 
-<body>
+<body id="entire>
     
   <div class="topnav" id="myTopnav">
       <a href="../../index.php" class="active">RIVAS PINESTRAW</a>
