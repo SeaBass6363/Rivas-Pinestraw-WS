fd16e27786c7301636b10eb56648284373e4ceec
Hardwood Vazquez <s.vazquez6363@gmail.com>
2024-04-16 00:34:24 -0400

    Add Admin page

--- src/pages/orders.php
+++ src/pages/orders.php
@@ -24,6 +24,7 @@ define( 'DB_HOST', 'localhost' );
       <a href="./order-form.php">Order Form</a>
       <a href="./checklist.php">Checklist</a>
       <a href="./description.php">Description</a>
+      <a href="./orders.php">Admin</a>
       <a href="./sign-on.php">Sign In</a>
       <a href="javascript:void(0);" class="icon" onclick="hideNav()">
           <i class="fa fa-bars"></i>
